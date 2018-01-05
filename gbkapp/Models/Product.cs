5319e805ccf4d7a8c50b62c2a3db5d2236a1f37d
James Osborn <jamescarlosborn@gmail.com>
2018-01-05 15:18:18 -0800

    add Product model

---
+++ gbkapp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gbkapp.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+    }
+}
