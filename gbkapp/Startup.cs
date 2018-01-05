b5561c87bd9095461ca1a83eda2a2db070601cd1
James Osborn <jamescarlosborn@gmail.com>
2018-01-05 10:46:10 -0800

    submit broken code to Startup.cs, Configuration error.

--- gbkapp/Startup.cs
+++ gbkapp/Startup.cs
@@ -7,11 +7,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace gbkapp
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -39,13 +47,6 @@ namespace gbkapp
             {
                 await context.Response.WriteAsync("Hello World!");
             });
-        }
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-              .SetBasePath(env.ContentRootPath)
-              .AddEnvironmentVariables();
-            Configuration = builder.Build();
         }
     }
 }
