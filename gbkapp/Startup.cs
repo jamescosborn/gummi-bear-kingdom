8376a5757cb1288eebfdf5ea3417f2cde18ed3c5
James Osborn <jamescarlosborn@gmail.com>
2018-01-05 11:33:38 -0800

    fix configuration error in startup.cs

--- gbkapp/Startup.cs
+++ gbkapp/Startup.cs
@@ -8,18 +8,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace gbkapp
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-              .SetBasePath(env.ContentRootPath)
-              .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+			  .SetBasePath(env.ContentRootPath)
+			  .AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,20 +32,22 @@ namespace gbkapp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+			app.UseDeveloperExceptionPage(); //!!!
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute( name: "default",
+		                         template: "{controller=Home}/{action=Index}/{id?}");
+			});
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			loggerFactory.AddConsole();
+
+            //if (env.IsDevelopment())
+            //{
+                //app.UseDeveloperExceptionPage();
+            //}
 
             app.Run(async (context) =>
             {
@@ -50,4 +56,3 @@ namespace gbkapp
         }
     }
 }
-//test
