764b999399167efa6769062e6c80294ec58c098a
James Osborn <jamescarlosborn@gmail.com>
2018-01-07 20:26:44 -0800

    add package to csproj, update gbkingdomContext

--- gbkapp/Models/gbkingdomContext.cs
+++ gbkapp/Models/gbkingdomContext.cs
@@ -8,10 +8,25 @@ namespace gbkapp.Models
 {
     public class gbkingdomContext : DbContext
     {
+        public gbkingdomContext()
+        {
+        }
+
         public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
                 .UseMySql(@"Server=localhost;Port=8889;database=gbkingdom;uid=root;pwd=root;");
+
+
+        public gbkingdomContext(DbContextOptions<gbkingdomContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
