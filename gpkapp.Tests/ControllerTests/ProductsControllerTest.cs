b587a55aa8393f162717d98555e70fe7f8c5248a
James Osborn <jamescarlosborn@gmail.com>
2018-01-12 15:37:48 -0800

    add ProductsControllerTest with first test.

---
+++ gpkapp.Tests/ControllerTests/ProductsControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace gpkapp.Tests.ControllerTests
+{
+    [TestMethod]
+    public void ProductsController_AddsProductToIndexModelData_Collection()
+    {
+        // Arrange
+        ProductsController controller = new ProductsController();
+        Product testProduct = new Product();
+        testProduct.Description = "test item";
+
+        // Act
+        controller.Create(testProduct);
+        ViewResult indexView = new ProductsController().Index() as ViewResult;
+        var collection = indexView.ViewData.Model as List<Product>;
+
+        // Assert
+        CollectionAssert.Contains(collection, testProduct);
+    }
