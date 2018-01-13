255a8bf2d29c9b26363fd0db3712495c17b69750
James Osborn <jamescarlosborn@gmail.com>
2018-01-12 16:24:26 -0800

    add Models/Review.cs

---
+++ gbkapp/Models/Review.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gbkapp.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string Author { get; set; }
+        public string ContentBody { get; set; }
+        public int Rating { get; set; }
+        public int ProductId { get; set; }
+    }
+
+    public override bool Equals(System.Object otherReview)
+    {
+        if (!(otherReview is Review))
+        {
+            return false;
+        }
+        else
+        {
+            Review newReview = (Review)otherReview;
+            return this.ReviewId.Equals(newReview.ReviewId);
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return this.ReviewId.GetHashCode();
+    }
+}
